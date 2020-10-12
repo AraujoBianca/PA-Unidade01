45a1803396f77bd1a5f936bfbd66ef7369028dfc
Bianca Araújo <54210222+AraujoBianca@users.noreply.github.com>
2020-10-12 20:32:32 -0300

    Update Q13 e Q14

--- Questao13.c
+++ Questao13.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int n, i;
+    int n, i, j;
+    float *vet, aux;
     printf("Quantos valores serão digitados? ");
     scanf("%d", &n);
 
-    float vet[n];
+    vet = malloc(n*sizeof(float));
 
     for(i = 0; i < n; i++){
+        printf("Digite o valor %d: ", i+1);
         scanf("%f", &vet[i]);
     }
 
     printf("\n");
 
+    for (i=0 ; i < n ; i++){
+       for (j = i; j < n; j++){
+        if(vet[i]>vet[j]){
+            aux = vet[i];
+            vet[i] = vet[j];
+            vet[j] = aux;
+        }
+      }
+    }
+
     for(i = 0; i < n; i++){
         printf("%f ", vet[i]);
     }
 
+    free(vet);
     return 0;
 }
--- Questao14.c
+++ Questao14.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compararCrescente(const void * a, const void * b){
+    if (*(float*)a == *(float*)b){ /* Nessa linha eu converto o void * para p tipo do vetor para o qsort que é float, se repete abaixo*/
+        return 0;
+    } else {
+        if (*(float*)a < *(float*)b){
+            return -1; /* a vem antes que b */
+        } else {
+            return 1; /* a vem depois que b */
+        }
+    }
+}
 
 int main(){
+    int n, i;
+
+    printf("Quantos valores serão digitados? ");
+    scanf("%d", &n);
+
+    float vet[n];
+
+    for (i = 0 ; i < n ; i++){
+        printf("Digite o valor %d: ", i+1);
+        scanf("%f", &vet[i]);
+    }
+
+    qsort(vet, n, sizeof(float), compararCrescente);
+
+    printf("Vetor ordenador: ");
+    for (i = 0 ; i < n ; i++){
+        printf("%f ", vet[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
