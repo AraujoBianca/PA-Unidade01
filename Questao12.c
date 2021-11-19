eb7b6078210b2d3c2bb2e0e9be88c8a617b095c9
Bianca Araújo <54210222+AraujoBianca@users.noreply.github.com>
2021-11-19 07:55:43 -0300

    Update Questao12.c

--- Questao12.c
+++ Questao12.c
@@ -4,7 +4,7 @@
 void pintarParede(float larg, float h, float metros);
 
 int main(){
-    setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL,"Portuguese"); /***/
     printf("12. O que é um ponteiro para uma função? Pesquise na Internet referências sobre o assunto e escreva um pequeno programa exemplificando o uso deste recurso.\n\n");
     printf("Ponteiro para função é uma forma de definir em tempo de execução qual função será executada sem precisar escrever o nome da função de forma explícita naquele ponto.\n\n");
 
